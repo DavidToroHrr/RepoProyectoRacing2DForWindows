e7847bc47ec91efec9499808ff222d20acfd17c5
Santiago <santicasta1003@gmail.com>
2024-05-01 17:39:15 -0500

    Se agrego el paquete models y manager

---
+++ src/proyectoracing2dforwindows/managers/FileManager.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoracing2dforwindows.managers;
+
+/**
+ *
+ * @author usuario
+ */
+public class FileManager {
+    
+}
